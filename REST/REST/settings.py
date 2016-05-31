d875821615bcdd7d050d1707bafe06c9440f8bd0
Lowell List <lowell.list@gmail.com>
2016-05-31 16:31:30 -0700

    removed session authentication

--- REST/REST/settings.py
+++ REST/REST/settings.py
@@ -146,7 +146,6 @@ ACCOUNT_USERNAME_REQUIRED = False
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
     )
 }
