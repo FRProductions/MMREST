6b28c9636f4a504b664d179407c8a1f8df1ccdc0
Lowell List <lowell.list@gmail.com>
2016-05-31 09:44:48 -0700

    customized emails for account confirmation, password reset

--- REST/REST/urls.py
+++ REST/REST/urls.py
@@ -14,6 +14,12 @@ urlpatterns = [
     # this one doesn't seem to do anything but needs to be present
     url(r'^rest-auth/registration/account-email-verification-sent/$', TemplateView.as_view(),
         name='account_email_verification_sent'),
+
+    # this url is used to generate email content
+    url(r'^password-reset/confirm/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        TemplateView.as_view(template_name="password_reset_confirm.html"),
+        name='password_reset_confirm'),
+
     url(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'^rest-auth/facebook/$', FacebookLogin.as_view(), name='fb_login'),
 ]
