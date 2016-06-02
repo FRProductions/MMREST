19a964ba0d2faf4e2d15d92b99141ae28e978bc5
Lowell List <lowell.list@gmail.com>
2016-06-02 09:30:52 -0700

    added workaround to avoid FB endpoint issue

--- REST/REST/views.py
+++ REST/REST/views.py
@@ -2,5 +2,11 @@ from allauth.socialaccount.providers.facebook.views import FacebookOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
 
 
+# workaround according to https://github.com/Tivix/django-rest-auth/issues/195
+class FacebookOAuth2AdapterFixed(FacebookOAuth2Adapter):
+    def __init__(self):
+        pass
+
+
 class FacebookLogin(SocialLoginView):
-    adapter_class = FacebookOAuth2Adapter
+    adapter_class = FacebookOAuth2AdapterFixed
