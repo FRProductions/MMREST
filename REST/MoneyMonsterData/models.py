392e935fd0d53ba103c3584639b7fb3dc3ed6f70
Lowell List <lowell.list@gmail.com>
2016-06-15 10:39:01 -0700

    added logic to CommentSerializer to validate fields and create new comments

--- REST/MoneyMonsterData/models.py
+++ REST/MoneyMonsterData/models.py
@@ -35,13 +35,13 @@ class VideoStatus(models.Model):
 
 # a user comment, which may be attached to other models (video, quiz, another comment, etc.)
 class Comment(models.Model):
-    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, null=True, blank=True,
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE,
                                      limit_choices_to=
                                      models.Q(app_label='MoneyMonsterData', model='video') |
                                      models.Q(app_label='MoneyMonsterData', model='quiz') |
                                      models.Q(app_label='MoneyMonsterData', model='comment')
                                      )
-    object_id = models.PositiveIntegerField(null=True, blank=True)
+    object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey('content_type', 'object_id')
     owner = models.ForeignKey(User)
     text = models.TextField(blank=False, max_length=1000)
--- REST/MoneyMonsterData/serializers.py
+++ REST/MoneyMonsterData/serializers.py
@@ -1,3 +1,5 @@
+from django.apps import apps
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import serializers
 
 from .models import *
@@ -69,10 +71,45 @@ class CommentLikeSerializer(serializers.HyperlinkedModelSerializer):
 class CommentSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
     like_count = serializers.IntegerField(source='like_parent.count', read_only=True)
+    content_type = serializers.CharField(source='content_type.model')
 
     class Meta:
         model = Comment
-        fields = ('url', 'text', 'date_added', 'owner', 'like_count')
+        fields = ('url', 'content_type', 'object_id', 'text', 'date_added', 'owner', 'like_count')
+
+    def validate_content_type(self, value):
+        """
+        Check that the content_type serialized value is a valid model name for this app
+        """
+        try:
+            app_label = self.Meta.model._meta.app_label
+            apps.get_model(app_label=app_label, model_name=value)  # Raises LookupError if model not found
+            return value
+        except:
+            raise serializers.ValidationError("not a valid model name")
+
+    def validate(self, data):
+        """
+        Check that the object_id refers to a valid object.
+        """
+        app_label = self.Meta.model._meta.app_label                             # label of this app
+        model_name = data['content_type']['model']
+        model = apps.get_model(app_label=app_label, model_name=model_name)      # content_type already validated
+        object_id = data['object_id']
+        try:
+            model.objects.get(id=object_id)                                     # attempt lookup
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError("no %s found with id %i" % (model_name, object_id))
+        return data
+
+    def create(self, validated_data):
+        """
+        Replace 'content_type' model name with real ContentType object
+        """
+        app_label = self.Meta.model._meta.app_label
+        model = apps.get_model(app_label=app_label, model_name=validated_data['content_type']['model'])
+        validated_data['content_type'] = ContentType.objects.get_for_model(model)
+        return Comment.objects.create(**validated_data)
 
 
 class VideoDataSerializer(serializers.HyperlinkedModelSerializer):
